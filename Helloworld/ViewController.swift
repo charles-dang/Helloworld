43cf0c2c9824ca7930fd52dfadb5cd8910aba982
Charles Dang <charles.d.dang@gmail.com>
2016-02-25 16:08:05 -0800

    initial checkin
    initial check-ing

---
+++ Helloworld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Helloworld
+//
+//  Created by Charles Dang on 2/16/16.
+//  Copyright © 2016 DanguardTutorials. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var DisplayLabel: UILabel!
+    @IBAction func button_click(sender: UIButton) {
+        DisplayLabel.text = "Hello World"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
